fbbf6408a8e5ed350d4fcaad8774fe03d77e835c
Varunesh07 <varunesh2007.tmlzs@gmail.com>
2025-10-07 23:01:49 +0530

    Added 6th table for admin details

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const { createPlayerTable } = require("./models/playerModel");
 const { createTableCoach } = require('./models/coachModel');
 const { createMatchLog } = require('./models/matchLogModel');
 const { createTableEval } = require('./models/evalModel');
-
+const { createTableAdmin } = require('./models/adminModel')
 
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+
+(async () => {
+  await createTableAdmin();
+})();
+
 
 (async () => {
   await createTableTeam();
